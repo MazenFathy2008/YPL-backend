0727390c0951898a4b711c62fae90ba4a3681e96
mazenDev <mazenfathy841@gmail.com>
2026-09-14 07:05:36 +0300

    setting up main middlewares and creating env.js

---
+++ config/env.js
@@ -0,0 +1,4 @@
+import "dotenv/config";
+
+export const FRONTEND_URL = process.env.FRONTEND_URL;
+export const PORT = process.env.PORT;
--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { PORT, FRONTEND_URL } from "./config/env.js";
 const app = express();
 
-const PORT = 5500;
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: FRONTEND_URL,
+    credentials: true,
+  }),
+);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
