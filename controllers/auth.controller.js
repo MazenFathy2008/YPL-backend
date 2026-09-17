b59796b58817138bc910316d25ab620e0f07b20d
mazenDev <mazenfathy841@gmail.com>
2026-09-17 15:37:06 +0300

    adding log out

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -104,3 +104,20 @@ export const getCurrentUser = async (req, res, next) => {
     next(error);
   }
 };
+
+export const logout = async (req, res, next) => {
+  try {
+    res.clearCookie("token", {
+      httpOnly: true,
+      secure: false,
+      sameSite: "lax",
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "Logged out successfully",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,11 @@
 import express from "express";
-import { login, getCurrentUser } from "../controllers/auth.controller.js";
+import {
+  login,
+  getCurrentUser,
+  logout,
+} from "../controllers/auth.controller.js";
 const router = express.Router();
 router.post("/login", login);
 router.get("/me", getCurrentUser);
+router.post("/logout", logout);
 export default router;
